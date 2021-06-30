1ba38d8405a469aa6122846a3b529c76b019925b
AgneSva <agnesvambaryte@gmail.com>
2021-07-01 00:14:10 +0300

    Corrections

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 
 x=[]
+
 #each line is saved
 with open('data.txt') as f:
     lines = [line.rstrip() for line in f]
 
 #converting sequences to color names
-
 for line in lines:
     if line=="0,0,1,0":
         x.append('green')
@@ -18,11 +18,10 @@ for line in lines:
     elif line=="0,0,0,0":
         x.append('no')
     else:
-        print("not correct sequence ")
+        print("not correct sequence")
+        
         
 
-#print(x)
-    
 for i in range(len(x)-1):
     
     current=x[i]
@@ -30,20 +29,19 @@ for i in range(len(x)-1):
     print("current:",current)
     print("next:",later)
     
-    if current=="green" and (later=='green' or later=='yellow' or later=='no'):
-        
+    if current=="green" and (later=='green' or later=='yellow' or later=='no'): 
             continue
 
     elif current=="red" and (later=='yellow'or  later=='red' or later=='no'):
-        
             continue
 
-    elif current=="yellow" and (later=='green'or  later=='arrow'or later=='no' or later=='yellow'):
-        
+    elif current=="yellow" and (later=='green'or  later=='arrow'or later=='no' or later=='yellow' or later=='red'):
             continue
     elif current=='no':
         continue
+    elif current=='arrow' and (later=='green' or later=='arrow'or later=='no') :
+        continue
 
     else:
-        print("breaking")
+        print("invalid")
         break

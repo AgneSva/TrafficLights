8dbaac837623687f9ffd4588d73f63581beafc33
AgneSva <agnesvambaryte@gmail.com>
2021-07-02 11:10:29 +0300

    Correction

--- Main.py
+++ Main.py
@@ -36,7 +36,7 @@ for i in range(len(x)-1):
         print("Traffic light is working fine")
     
     #after green possible sequences
-    if current=="green" and (later=='green' or later=='yellow' or later=='no'): 
+    if current=="green" and (later=='green' or later=='yellow' or later=='no'or later=='arrow'): 
             continue
 
     #after red possible sequences
